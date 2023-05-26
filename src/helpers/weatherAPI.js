462cfcc807afee244f46969399866e4e7585ce70
Ana Carolina Rodrigues <carolina_ralves@hotmail.com>
2023-05-25 23:45:13 -0300

    Requisito 1!!!!

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,13 @@
-// Remova os comentários a medida que for implementando as funções
+const token = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = async (term) => {
+  const apiURL = `http://api.weatherapi.com/v1/search.json?lang=pt&key=${token}&q=${term}`;
+  const response = await fetch(apiURL);
+  const data = await response.json();
+  if (data.length === 0) {
+    return alert('Nenhuma cidade encontrada');
+  }
+  return data;
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
